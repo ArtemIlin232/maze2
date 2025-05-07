26eecca63aa9d20da830a859ea331123bd27e033
ArtemIlin232 <artyom.ilin.08@mail.ru>
2025-05-07 18:26:25 +0300

    доделать

---
+++ my_app.py
@@ -0,0 +1,17 @@
+class MainWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+    def initUI(self):
+        self.hello_text = QLabel(txt_hello)
+        self.instruction = QLabel(txt_instruction)
+        self.button = QPushButton(txt_next)
+    def connects(self):
+        pass
