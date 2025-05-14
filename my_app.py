7741963af86925a7e1e46e6e29295718814fb5e6
ArtemIlin232 <artyom.ilin.08@mail.ru>
2025-05-14 18:34:24 +0300

    2

--- my_app.py
+++ my_app.py
@@ -1,3 +1,13 @@
+from PyQt5.QtCore import Qt, QTimer, QTime, QLocale
+from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFont 
+from PyQt5.QtWidgets import (
+        QApplication, QWidget, 
+        QHBoxLayout, QVBoxLayout, QGridLayout, 
+        QGroupBox, QRadioButton,
+        QPushButton, QLabel, QListWidget, QLineEdit)
+
+from second import *
+from instr import *
 class MainWin(QWidget):
     def __init__(self):
         super().__init__()
@@ -10,8 +20,21 @@ class MainWin(QWidget):
         self.resize(win_width, win_height)
         self.move(win_x, win_y)
     def initUI(self):
+        self.btn_next = QPushButton(txt_next, self)
         self.hello_text = QLabel(txt_hello)
         self.instruction = QLabel(txt_instruction)
-        self.button = QPushButton(txt_next)
+
+        self.layout_line = QVBoxLayout()
+        self.layout_line.addWidget(self.hello_text, alignment = Qt.AlignLeft)
+        self.layout_line.addWidget(self.instruction, alignment = Qt.AlignLeft) 
+        self.layout_line.addWidget(self.btn_next, alignment = Qt.AlignCenter)          
+        self.setLayout(self.layout_line)
     def connects(self):
-        pass
+        self.btn_next.clicked.connect(self.next_click)
+    def next_click(self):
+        self.tw = TestWin()
+        self.hide()
+
+app = QApplication([])
+mw = MainWin()
+app.exec_()
